6e52091045eb2b0029d082842f76b9898082d232
Victoria123432 <vikabaravika007@gmail.com>
2024-01-11 00:57:44 +0200

    11.01.24

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$(function () {
+	var header = $('#header'),
+		introH = $('#intro').innerHeight(),
+		scrollOffset = 0
+	$(window).on('scroll', function () {
+		scrollOffset = $(this).scrollTop()
+	})
+})
