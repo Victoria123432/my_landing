f8df25669670cc3f2509fc780afdedfa46551260
Victoria123432 <vikabaravika007@gmail.com>
2024-01-13 23:59:37 +0200

    13.01.24

--- js/app.js
+++ js/app.js
@@ -1,8 +1,73 @@
 $(function () {
 	var header = $('#header'),
 		introH = $('#intro').innerHeight(),
-		scrollOffset = 0
+		scrollOffset = $(window).scrollTop()
+	/*Header fixed*/
+	checkScroll(scrollOffset)
+
 	$(window).on('scroll', function () {
 		scrollOffset = $(this).scrollTop()
+		checkScroll(scrollOffset)
+	})
+	function checkScroll(scrollOffset) {
+		if (scrollOffset >= introH) {
+			header.addClass('fixed')
+		} else {
+			header.removeClass('fixed')
+		}
+	}
+
+	/* Smooth scroll*/
+	$('[data-scroll]').on('click', function (event) {
+		event.preventDefault()
+
+		var $this = $(this),
+			blockId = $(this).data('scroll'),
+			blockOffSet = $(blockId).offset().top
+
+		$('nav a').removeClass('active')
+		$this.addClass('active')
+
+		$('html, body').animate(
+			{
+				scrollTop: blockOffSet,
+			},
+			500,
+		)
+	})
+
+	/*Menu nav toggle*/
+
+	$('#nav_toggle').on('click', function (event) {
+		event.preventDefault()
+
+		$(this).toggleClass('active')
+
+		$('#nav').toggleClass('active')
+	})
+	/*Menu disappear*/
+	$('nav a').on('click', function (event) {
+		event.preventDefault()
+
+		$('#nav').removeClass('active')
+		$('#nav_toggle').removeClass('active')
+	})
+
+	/* Collapse*/
+	$('[data-collapse]').on('click', function (event) {
+		event.preventDefault()
+
+		var $this = $(this),
+			blockId = $this.data('collapse')
+
+		$this.toggleClass('active')
+	})
+
+	/*slider*/
+	$('[data-slider]').slick({
+		infinite: true,
+		fade: false,
+		slidesToShow: 1,
+		slidesToScroll: 1,
 	})
 })
